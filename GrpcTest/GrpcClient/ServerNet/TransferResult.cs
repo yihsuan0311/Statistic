f5cd756c9f67215d91cb1e709b502fc6b21d321c
yihsuan0311 <75189950+yihsuan0311@users.noreply.github.com>
2021-08-05 05:21:57 +0800

    Add files via upload

---
+++ GrpcTest/GrpcClient/ServerNet/TransferResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerNet
+{
+    /// <summary>
+    /// 传输结果
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class TransferResult<T>
+    {
+        /// <summary>
+        /// 传输是否成功
+        /// </summary>
+        public bool IsSuccessful { get; set; }
+        /// <summary>
+        /// 消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 标记类型
+        /// </summary>
+        public T Tag { get; set; } = default;
+    }
+}
